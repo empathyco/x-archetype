2d051f05c8063c937ef4464dbba38536ff357190
Pedro López Mareque <pedrol@empathy.co>
2021-06-09 10:58:06 +0200

    feat(adapter): update adapter's config in order to use the new platfo… (#2)
    feat(adapter): update adapter's config in order to use the new platform's api
    
    EX-3785

--- public/snippet-script.js
+++ public/snippet-script.js
@@ -1,40 +1,27 @@
 function getURLParameter(name) {
-    return decodeURIComponent(
-      (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(window.location.href) || [, ''])[1].replace(/\+/g, '%20')) || null;
+  return decodeURIComponent(
+    (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(window.location.href) || [, ''])[1].replace(/\+/g, '%20')) || null;
 }
 
-var instance = getURLParameter('instance') || 'juguettos';
+var instance = getURLParameter('instance') || undefined;
 var env = getURLParameter('env') || undefined;
-var scope = getURLParameter('scope') || 'default';
-var lang = getURLParameter('lang') || 'es';
+var scope = getURLParameter('scope') || 'desktop';
+var lang = getURLParameter('lang') || 'en';
 var device = getURLParameter('device') || 'mobile';
 var searchLang = getURLParameter('searchLang') || lang;
 var currency = getURLParameter('currency') || 'EUR';
 var consent = getURLParameter('consent') === 'true' || false;
 var documentDirection = getURLParameter('doc-dir') || 'ltr';
 
-/*window.initX = function() {
-  return {
-    instance,
-    env,
-    scope,
-    lang,
-    searchLang,
-    currency,
-    consent,
-    documentDirection
-  };
-};*/
-
 window.initX = {
-    instance,
-    env,
-    scope,
-    lang,
-    device,
-    searchLang,
-    currency,
-    consent,
-    documentDirection
+  instance,
+  env,
+  scope,
+  lang,
+  device,
+  searchLang,
+  currency,
+  consent,
+  documentDirection
 };
 
---
+++ src/adapters/demo-result.mapper.ts
@@ -0,0 +1,65 @@
+import { EmpathyResult } from '@empathy/search-adapter';
+import { Result } from '@empathy/search-types';
+
+/**
+ * Custom platform result which extends the base {@link Result}.
+ */
+interface CustomResult {
+  /** Gender of the result. */
+  gender: string;
+  /** Color of the result. */
+  color: string;
+  /** Year of the result. */
+  year: string;
+  /** List of category paths of the result. */
+  categoryPaths: string[];
+  /** Usage of the result. */
+  usage: string;
+  /** Group ID of the result. */
+  groupId: string;
+  /** Type of the result. */
+  type: string;
+  /** Score of the result. */
+  score: number;
+  /** List of category ids of the result. */
+  categoryIds: string[];
+  /** Size of the result. */
+  size: string;
+  /** Season of the result. */
+  season: string;
+  /** List of categories of the result. */
+  categories: string[];
+}
+
+declare module '@empathy/search-types' {
+  interface Result extends CustomResult {}
+}
+
+declare module '@empathy/search-adapter' {
+  interface EmpathyResult extends CustomResult {}
+}
+
+/**
+ * This mapper adapts the response from the api to X Components.
+ *
+ * @param rawResult - This is the raw response form the API.
+ * @param result - This is the Empathy result handle by the app.
+ *
+ * @returns A new object which can be handled by XComponents.
+ */
+export function resultMapper(rawResult: EmpathyResult, result: Result): Result {
+  return Object.assign<Result, Partial<Result>>(result, {
+    gender: rawResult.gender,
+    color: rawResult.color,
+    year: rawResult.year,
+    usage: rawResult.usage,
+    groupId: rawResult.groupId,
+    type: rawResult.type,
+    score: rawResult.score,
+    size: rawResult.size,
+    season: rawResult.season,
+    categories: rawResult.categories,
+    categoryIds: rawResult.categoryIds,
+    categoryPaths: rawResult.categoryPaths
+  });
+}
