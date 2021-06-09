f1960ef463982d40507b45f63938087c0fff7ab2
Pedro Lopez Mareque <pedrol@empathy.co>
2021-06-09 08:10:12 +0200

    [EX-3785] fix pr tasks

--- src/adapters/demo-result.mapper.ts
+++ src/adapters/demo-result.mapper.ts
@@ -1,20 +1,37 @@
 import { EmpathyResult } from '@empathy/search-adapter';
 import { Result } from '@empathy/search-types';
 
-type CustomResult = {
-  gender: string;
-  color: string;
-  year: string;
-  categoryPaths: string[];
-  usage: string;
-  groupId: string;
-  type: string;
-  score: number;
-  categoryIds: string[];
-  size: string;
-  season: string;
-  categories: string[];
-};
+/**
+ * Custom platform result which extends the base {@link Result}.
+ */
+declare module '@empathy/search-types' {
+  interface Result {
+    /** Gender of the result. */
+    gender: string;
+    /** Color of the result. */
+    color: string;
+    /** Year of the result. */
+    year: string;
+    /** List of category paths of the result. */
+    categoryPaths: string[];
+    /** Usage of the result. */
+    usage: string;
+    /** Group ID of the result. */
+    groupId: string;
+    /** Type of the result. */
+    type: string;
+    /** Score of the result. */
+    score: number;
+    /** List of category ids of the result. */
+    categoryIds: string[];
+    /** Size of the result. */
+    size: string;
+    /** Season of the result. */
+    season: string;
+    /** List of categories of the result. */
+    categories: string[];
+  }
+}
 
 /**
  * This mapper adapts the response from the api to X Components.
@@ -24,10 +41,7 @@ type CustomResult = {
  *
  * @returns A new object which can be handled by XComponents.
  */
-export function resultMapper(
-  rawResult: Result & CustomResult,
-  result: EmpathyResult & CustomResult
-): EmpathyResult {
+export function resultMapper(rawResult: Result, result: EmpathyResult): EmpathyResult {
   return Object.assign(result, {
     gender: rawResult.gender,
     color: rawResult.color,
