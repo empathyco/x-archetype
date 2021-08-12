77f88a12671332e584ad99518422ec3021f8e58b
Guillermo Cacheda <guillermoc@MacBook-Pro-de-Guillermo.local>
2021-08-12 08:38:52 +0200

    [EX-4548] Remove comment about external project

--- src/i18n/currencies.ts
+++ src/i18n/currencies.ts
@@ -1,7 +1,3 @@
-/**
- * TODO: This configuration should be part of an intermediate project that would be imported by
- * the X-Archetype.
- */
 export default {
   EUR: 'i.iii,dd €',
   USD: '$i,iii.dd'
