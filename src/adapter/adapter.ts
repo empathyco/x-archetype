25bb4659ac8e2b7dc232a039c2151466ab1a0277
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2021-11-24 16:58:25 +0100

    chore: undo change

--- src/adapter/adapter.ts
+++ src/adapter/adapter.ts
@@ -32,7 +32,13 @@ export const adapter = new EmpathyAdapterBuilder()
   .setFeatureConfig('search', {
     endpoint: '{env}/query/empathy/search',
     responsePaths: {
-      results: 'catalog.content'
+      results: 'catalog.content',
+      facets: 'catalog.facets',
+      totalResults: 'catalog.numFound',
+      spellcheck: 'catalog.spellchecked',
+      banners: 'banner.content',
+      promoteds: 'promoted.content',
+      redirections: 'direct.content'
     }
   })
   .setFeatureConfig('searchById', {
