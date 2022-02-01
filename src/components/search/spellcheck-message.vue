165ab53ee81b7aa7e21edaf21b835a5f94bc1bb0
Diego Pascual <diegopf@users.noreply.github.com>
2022-02-01 18:11:52 +0100

    refactor: remove mixin from spellcheck

--- src/components/search/spellcheck-message.vue
+++ src/components/search/spellcheck-message.vue
@@ -1,16 +1,15 @@
 <template>
-  <Spellcheck v-if="hasSearched && $x.totalResults > 0" #default="{ query }" class="x-message">
+  <Spellcheck v-if="$x.totalResults > 0" #default="{ query }" class="x-message">
     <p>
       <span v-html="$t('spellcheck.message', { query })" class="x-text" />
-      <SpellcheckButton v-if="hasSearched" class="x-button x-button--ghost x-padding--01" />
+      <SpellcheckButton class="x-button x-button--ghost x-padding--01" />
     </p>
   </Spellcheck>
 </template>
 
 <script lang="ts">
-  import { Component } from 'vue-property-decorator';
+  import { Component, Vue } from 'vue-property-decorator';
   import { Spellcheck, SpellcheckButton } from '@empathyco/x-components/search';
-  import HasSearchedMixin from '../has-searched.mixin.vue';
 
   @Component({
     components: {
@@ -18,5 +17,5 @@
       SpellcheckButton
     }
   })
-  export default class SpellcheckMessage extends HasSearchedMixin {}
+  export default class SpellcheckMessage extends Vue {}
 </script>
