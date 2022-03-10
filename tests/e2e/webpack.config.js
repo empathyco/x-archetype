e55e27dcd97dfc82b58e4a9599f239977c804b44
Guillermo Cacheda <guillermoc@MacBook-Pro-de-Guillermo.local>
2022-03-10 08:00:06 +0100

    WIP fix for Cypress and webpack v5

--- tests/e2e/webpack.config.js
+++ tests/e2e/webpack.config.js
@@ -1,8 +1,11 @@
+const { DefinePlugin } = require('webpack');
+
 module.exports = {
+  mode: 'production',
   resolve: {
-    extensions: ['.ts', '.js', '.feature', '.features']
+    extensions: ['.ts', '.js', '.feature', '.features'],
+    fallback: { "path": false, fs: 'false', child_process: 'false', readline: 'false' }
   },
-  node: { fs: 'empty', child_process: 'empty', readline: 'empty' },
   module: {
     rules: [
       {
@@ -38,5 +41,10 @@ module.exports = {
         use: ['babel-loader']
       }
     ]
-  }
+  },
+  plugins: [
+      new DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      })
+  ]
 };
