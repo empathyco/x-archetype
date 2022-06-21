e8df7550acaecd024014ec84d82a146a440142f4
manuelnavarro <manueln@empathy.co>
2022-06-21 14:57:32 +0200

    feat: update search box style and config
    EX-6340

--- src/components/search-box.vue
+++ src/components/search-box.vue
@@ -1,23 +1,36 @@
 <template>
-  <div
-    class="x-input-group"
-    :class="$x.device === 'mobile' ? 'x-input-group--card' : 'x-input-group--line'"
-  >
-    <SearchIcon />
-    <SearchInput :placeholder="$t('searchBox.placeholder')" />
-    <ClearSearchInput>{{ $t('searchBox.clear') }}</ClearSearchInput>
+  <div class="x-input-group x-input-group--card">
+    <SearchInput
+      :placeholder="$t('searchBox.placeholder')"
+      :class="$x.device === 'mobile' ? 'x-padding--left-05' : 'x-padding--left-08'"
+    />
+
+    <ClearSearchInput v-if="$x.device === 'desktop' && $x.query.searchBox" class="x-font-size--04">
+      {{ $t('searchBox.clear') }}
+    </ClearSearchInput>
+
+    <button
+      v-if="$x.device === 'desktop' || !$x.query.searchBox"
+      class="x-button x-input-group__action"
+    >
+      <SearchIcon />
+    </button>
+    <ClearSearchInput v-else class="x-input-group__action">
+      <CrossTinyIcon class="x-icon--l" />
+    </ClearSearchInput>
   </div>
 </template>
 
 <script lang="ts">
   import { ClearSearchInput, SearchButton, SearchInput } from '@empathyco/x-components/search-box';
-  import { SearchIcon } from '@empathyco/x-components';
+  import { SearchIcon, CrossTinyIcon } from '@empathyco/x-components';
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
 
   @Component({
     components: {
       ClearSearchInput,
+      CrossTinyIcon,
       SearchButton,
       SearchIcon,
       SearchInput
