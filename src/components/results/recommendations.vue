850036d5a4a9332aefccce3984c19adb9222c2e6
manuelnavarro <manueln@empathy.co>
2022-06-22 17:14:38 +0200

    fix: restore recommendations component for no results

--- src/components/results/recommendations.vue
+++ src/components/results/recommendations.vue
@@ -1,24 +1,25 @@
 <template>
   <div
-    v-if="!$x.query.searchBox && $x.recommendations.length > 0"
+    v-if="$x.recommendations.length > 0"
     class="x-list x-list--align-center x-list--padding-07 x-list--gap-07 x-list--padding-bottom"
   >
     <h1 class="x-title1 x-text--bold">{{ $t('recommendations.title') }}</h1>
-    <SlidingPanel
-      class="x-sliding-panel--buttons-overflow x-sliding-panel--show-buttons-on-hover"
-      :showButtons="$x.device !== 'mobile'"
-    >
-      <Recommendations v-if="!$x.totalResults">
-        <template #default="{ recommendation }">
-          <Result :result="recommendation" data-test="recommendation-item" />
-        </template>
-      </Recommendations>
-    </SlidingPanel>
+    <Recommendations v-if="!$x.totalResults">
+      <template #layout="{ recommendations }">
+        <BaseGrid
+          #default="{ item: result }"
+          :animation="staggeredFadeAndSlide"
+          :items="recommendations"
+        >
+          <Result :result="result" data-test="recommendation-item" />
+        </BaseGrid>
+      </template>
+    </Recommendations>
   </div>
 </template>
 
 <script lang="ts">
-  import { BaseGrid, SlidingPanel } from '@empathyco/x-components';
+  import { BaseGrid, StaggeredFadeAndSlide } from '@empathyco/x-components';
   import { Recommendations } from '@empathyco/x-components/recommendations';
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
@@ -28,9 +29,10 @@
     components: {
       BaseGrid,
       Recommendations,
-      Result,
-      SlidingPanel
+      Result
     }
   })
-  export default class Results extends Vue {}
+  export default class Results extends Vue {
+    protected staggeredFadeAndSlide = StaggeredFadeAndSlide;
+  }
 </script>
