39ccaf9c0b892cd54c3ccba51196a707edf0a0e7
manuelnavarro <manueln@empathy.co>
2022-06-22 10:25:34 +0200

    fix pr comments

--- src/components/search-box.vue
+++ src/components/search-box.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="x-input-group x-input-group--card">
+  <div class="x-input-group x-input-group--card x-border-color--lead">
     <SearchInput
       :placeholder="$t('searchBox.placeholder')"
       :class="$x.device === 'mobile' ? 'x-padding--left-05' : 'x-padding--left-08'"
