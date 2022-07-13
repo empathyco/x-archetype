cbc466cbede92dea6fe9e4c04e63ebec99f23c03
Diego Pascual <diegopf@users.noreply.github.com>
2022-07-13 12:24:12 +0200

    feat: add `MyHistory` aside (#144)
    EX-6512

---
+++ src/components/my-history/desktop-my-history-aside.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="x-background--neutral-100 x-list__item--expand x-list x-list--vertical">
+    <div class="x-list x-list--align-stretch x-border-color--neutral-10 x-background--neutral-10">
+      <div class="x-list x-list--horizontal x-list--align-center x-padding--07">
+        <h1 class="x-title2 x-font-color--neutral-100 x-font-weight--bold x-margin--right-auto">
+          {{ $t('myHistory.title') }}
+        </h1>
+        <BaseIdModalClose
+          class="
+            x-border-width--00
+            x-button--ghost x-button--ghost-start
+            x-padding--top-00 x-padding--bottom-00
+          "
+          modalId="my-history-aside"
+        >
+          <CrossIcon class="x-icon--l x-font-color--neutral-100" />
+        </BaseIdModalClose>
+      </div>
+    </div>
+    <CustomMyHistory class="x-list x-list__item--expand" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { BaseIdModalClose, CrossIcon } from '@empathyco/x-components';
+  import { Component, Vue } from 'vue-property-decorator';
+  import CustomMyHistory from './custom-my-history.vue';
+
+  @Component({
+    components: {
+      BaseIdModalClose,
+      CrossIcon,
+      CustomMyHistory
+    }
+  })
+  export default class DesktopMyHistoryAside extends Vue {
+    mounted(): void {
+      const activeElement = document.activeElement as HTMLElement;
+      if (activeElement.classList.contains('x-search-input')) {
+        activeElement.blur();
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .x-button {
+    --x-size-height-button-default: auto;
+  }
+</style>
