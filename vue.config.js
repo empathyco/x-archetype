3b1455a1e75f4e003aa7f011af078f51323a7f9e
Diego Pascual <diegopf@users.noreply.github.com>
2022-09-30 11:34:31 +0200

    fix: filter `x-components` source maps as well

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,9 @@ module.exports = defineConfig({
     },
     output: {
       devtoolModuleFilenameTemplate: ({ resourcePath, hash, query }) =>
-        resourcePath.match(/\.vue$/) && !query.match(/type=script/)
+        resourcePath.match(/\.vue$/) &&
+        !query.match(/type=script/) &&
+        !query.match(/\?rollup-plugin-vue=script.ts/)
           ? `webpack-generated:///${resourcePath}?${hash}`
           : `sources://${resourcePath}`,
       devtoolFallbackModuleFilenameTemplate: 'webpack:///[resource-path]?[hash]'
