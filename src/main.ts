cf063fe97ee0b75f939423b7ec01200d06a079c5
Javier Iglesias García <Javi.igl.gar@gmail.com>
2022-10-12 21:30:07 +0200

    feat: enable `devtools` in Empathy environments

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { XInstaller } from '@empathyco/x-components';
 import Vue from 'vue';
 import { installXOptions } from './x-components/plugin.options';
 
+declare global {
+  interface Window {
+    __enableVueDevtools__?: boolean;
+  }
+}
+
 Vue.config.productionTip = false;
+Vue.config.devtools = window.__enableVueDevtools__ ?? false;
 
 new XInstaller(installXOptions).init();
