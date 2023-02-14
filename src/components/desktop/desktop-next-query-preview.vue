f089569f55b54c5e19a2256804e517f8445a090c
Abraham Javier Pérez Bautista <ajperezbau@protonmail.com>
2023-02-14 11:10:14 +0000

    feat(next-queries): change view all button
    EX-7325

--- src/components/desktop/desktop-next-query-preview.vue
+++ src/components/desktop/desktop-next-query-preview.vue
@@ -9,22 +9,21 @@
         <span class="x-title3">{{ $x.query.nextQueries }}</span>
       </template>
     </i18n>
-    <div class="x-list x-list--horizontal x-list--align-baseline x-list--gap-02">
-      <span class="x-title3">
+    <NextQuery :suggestion="nextQuery" class="x-suggestion-lead-50 x-font-bold">
+      <span>
         {{
           $t('nextQueryPreview.query', {
             query: suggestion.query
           })
         }}
-      </span>
-      <span class="x-text1">
         {{
           $t('nextQueryPreview.totalResults', {
             totalResults: totalResults
           })
         }}
       </span>
-    </div>
+      <ArrowRightIcon class="x-icon-lg" />
+    </NextQuery>
     <BaseGrid
       #default="{ item }"
       :columns="maxItemsToRender"
@@ -33,18 +32,12 @@
     >
       <Result :result="item" />
     </BaseGrid>
-    <NextQuery
-      :suggestion="nextQuery"
-      class="x-tag x-tag--pill x-margin--left-auto x-margin--right-auto x-margin--top-03 x-padding--top-04 x-padding--bottom-04 x-padding--right-05 x-padding--left-05 x-font-bold"
-    >
-      {{ $t('nextQueryPreview.viewResults') }}
-    </NextQuery>
   </NextQueryPreview>
 </template>
 
 <script lang="ts">
   import { Component, Prop, Vue } from 'vue-property-decorator';
-  import { BaseGrid } from '@empathyco/x-components';
+  import { BaseGrid, ArrowRightIcon } from '@empathyco/x-components';
   import { NextQuery, NextQueryPreview } from '@empathyco/x-components/next-queries';
   import { NextQuery as NextQueryModel } from '@empathyco/x-types';
   import Result from '../results/result.vue';
@@ -54,12 +47,13 @@
       BaseGrid,
       NextQuery,
       NextQueryPreview,
-      Result
+      Result,
+      ArrowRightIcon
     }
   })
   export default class DesktopNextQueryPreview extends Vue {
     // TODO: Remove this and configure NextQueryPreview after doing EX-6819.
-    @Prop({ default: 4 })
+    @Prop({ default: 5 })
     protected maxItemsToRender!: number;
 
     @Prop({ required: true })
