2e404a87189fca638c6b556cbd8b2dad3029d245
Iván Tajes <tajespasarela@gmail.com>
2023-02-21 15:54:37 +0100

    remove unnecessary config

--- src/tailwind/plugin-options.js
+++ src/tailwind/plugin-options.js
@@ -1,11 +1,4 @@
 /** @type {import('@empathyco/x-tailwindcss').PluginOptions} */
 module.exports = {
-  theme: {
-    screens: {
-      // TODO: remove when changed in the default Theme in XDS
-      tablet: '744px',
-      desktop: '1280px',
-      large: '2560px'
-    }
-  }
+  theme: {}
 };
