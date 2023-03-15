6b95e340f2f85f85306e2dc4e5d7fca0fac96e5e
Manuel Navarro <manueln@empathy.co>
2023-03-15 15:19:51 +0100

    feat(design-system): update `tag` styles

--- src/components/search/facets/selected-filters.vue
+++ src/components/search/facets/selected-filters.vue
@@ -16,11 +16,11 @@
       >
         <template #default="{ filter }">
           <SimpleFilter
-            class="x-tag x-tag--pill x-tag--ghost x-background--auxiliary x-padding--04 x-padding--top-03 x-padding--bottom-03 x-text-auxiliary-50"
+            class="x-padding--04 x-padding--top-03 x-padding--bottom-03 x-tag x-tag-auxiliary x-tag-outlined x-selected x-rounded-full"
             :filter="filter"
           >
             <template #label="{ filter }">
-              <span class="x-title4">{{ filter.label }}</span>
+              {{ filter.label }}
               <CrossTinyIcon />
             </template>
           </SimpleFilter>
@@ -28,11 +28,11 @@
 
         <template #price="{ filter }">
           <NumberRangeFilter
-            class="x-tag x-tag--pill x-tag--ghost x-background--auxiliary x-padding--04 x-padding--top-03 x-padding--bottom-03 x-text-auxiliary-50"
+            class="x-padding--04 x-padding--top-03 x-padding--bottom-03 x-tag x-tag-auxiliary x-tag-outlined x-selected x-rounded-full"
             :filter="filter"
           >
             <template #label="{ filter }">
-              <PriceFilterLabel class="x-title4" :filter="filter" />
+              <PriceFilterLabel :filter="filter" />
               <CrossTinyIcon />
             </template>
           </NumberRangeFilter>
--- src/components/search/related-tags.vue
+++ src/components/search/related-tags.vue
@@ -2,7 +2,7 @@
   <SlidingPanel
     v-if="$x.relatedTags.length"
     class="x-sliding-panel-show-buttons-on-hover"
-    buttonClass="x-button-lead x-button-circle x-button-ghost x-padding--00"
+    buttonClass="x-tag x-tag-ghost x-padding--00"
     scrollContainerClass="desktop:x-sliding-panel-fade"
     :showButtons="$x.device !== 'mobile'"
   >
@@ -10,10 +10,11 @@
       <ChevronLeftIcon class="x-icon-lg" />
     </template>
     <RelatedTags
-      class="x-list--gap-03 x-tag--pill"
+      class="x-list--gap-03"
       :class="{ 'x-padding--left-05': $x.device === 'mobile' }"
       :highlightCurated="true"
       :animation="relatedTagsAnimation"
+      itemClass="x-tag-outlined x-rounded-full"
     >
       <template #related-tag-content="{ relatedTag, isSelected, shouldHighlightCurated }">
         <CuratedCheckIcon v-if="shouldHighlightCurated" />
