c6356b44ae478675de4383bf1384a2cfad809bb0
Iván Tajes <tajespasarela@gmail.com>
2023-03-30 14:28:48 +0200

    Updated x-tailwindcss version

--- src/tailwind/plugin-options.js
+++ src/tailwind/plugin-options.js
@@ -1,11 +1,4 @@
 /** @type {import('@empathyco/x-tailwindcss').PluginOptions} */
 module.exports = {
-  theme: {
-    extend: {
-      // TODO remove when new version of XDS plugin is used
-      borderWidth: {
-        DEFAULT: '1px'
-      }
-    }
-  }
+  theme: {}
 };
