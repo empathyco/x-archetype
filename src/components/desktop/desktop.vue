c5c50cbf046421be4ef87dacc5485ea009315e3c
Laura Martinez Garcia <lauram@empathy.co>
2023-06-06 11:36:41 +0200

    animation to collapse toolbar

--- src/components/desktop/desktop.vue
+++ src/components/desktop/desktop.vue
@@ -1,6 +1,9 @@
 <template>
   <div
     class="x-layout-container x-layout-max-width-md desktop:x-layout-min-margin-32 large:x-layout-max-width-lg large:x-layout-min-margin-48"
+    :class="{
+      'x-layout--is-scrolling-up': hasScrolled
+    }"
   >
     <div class="header x-layout-item x-pb-8">
       <header class="x-grid x-grid-cols-6 x-items-center x-gap-12 x-pt-24">
@@ -19,28 +22,32 @@
       </header>
     </div>
 
-    <div
-      class="x-layout__sub-header x-layout-item"
-      :class="{
-        'x-layout__sub-header--is-scrolling-down': hasScrolled
-      }"
-    >
-      <div class="x-grid x-grid-cols-6">
-        <LocationProvider location="predictive_layer" class="x-col-span-4 x-col-start-2">
-          <RelatedTags v-if="$x.relatedTags.length > 0" class="x-pb-24" />
-        </LocationProvider>
-      </div>
-
-      <div v-if="!$x.redirections.length && hasSearched">
-        <DesktopToolbar />
-      </div>
-
-      <div v-if="$x.totalResults > 0 && hasSearched && $x.selectedFilters.length">
-        <SelectedFilters class="x-py-16" />
+    <div class="x-layout__sub-header">
+      <div class="x-layout__sub-header-content x-layout-item">
+        <div class="x-grid x-grid-cols-6">
+          <LocationProvider location="predictive_layer" class="x-col-span-4 x-col-start-2">
+            <RelatedTags v-if="$x.relatedTags.length > 0" class="x-pb-24" />
+          </LocationProvider>
+        </div>
+        <div v-if="!$x.redirections.length && hasSearched">
+          <DesktopToolbar />
+        </div>
+        <div v-if="$x.totalResults > 0 && hasSearched && $x.selectedFilters.length">
+          <SelectedFilters class="x-py-16" />
+        </div>
       </div>
     </div>
 
-    <MainScroll class="x-flex x-flex-col">
+    <BaseIdModal
+      key="right-aside"
+      :animation="rightAsideAnimation"
+      modalId="right-aside"
+      contentClass="x-w-512 x-ml-auto"
+    >
+      <DesktopAside />
+    </BaseIdModal>
+
+    <MainScroll class="x-layout__main-scroll x-flex x-flex-col">
       <Scroll id="main-scroll">
         <div v-if="!$x.redirections.length && hasSearched" class="x-layout-item">
           <LocationProvider location="results">
@@ -61,15 +68,6 @@
         </div>
 
         <BaseIdModal
-          key="right-aside"
-          :animation="rightAsideAnimation"
-          modalId="right-aside"
-          contentClass="x-w-512 x-ml-auto"
-        >
-          <DesktopAside />
-        </BaseIdModal>
-
-        <BaseIdModal
           key="my-history-aside"
           :animation="rightAsideAnimation"
           modalId="my-history-aside"
@@ -80,6 +78,7 @@
         <MyHistoryConfirmDisableModal />
       </Scroll>
     </MainScroll>
+
     <div class="x-layout-on-margin-right x-layout-overlap x-layout-item">
       <ScrollToTop />
     </div>
@@ -164,14 +163,48 @@
   .x-layout-item > * {
     min-width: 0;
   }
-  .header {
+  /*.header {
     z-index: 1;
+    background-color: white;
   }
-  .x-layout__sub-header {
+  .x-layout--is-scrolling-up {
+    .x-layout__sub-header,
+    .x-layout__main-scroll {
+      transform: translateY(0%);
+    }
+  }
+  .x-layout__sub-header,
+  .x-layout__main-scroll {
     transition: transform 0.25s ease-out;
-    transform: translateY(-150%) !important;
+    transform: translateY(-160px);
   }
-  .x-layout__sub-header--is-scrolling-down {
-    transform: translateY(0%) !important;
+  .x-layout__main-scroll {
+    height: 100vh !important;
+  }*/
+
+  .x-layout__sub-header {
+    display: grid;
+    grid-template-rows: 0fr;
+    overflow: hidden;
+    transition: grid-template-rows 0.5s;
+    & > * {
+      transition: none;
+    }
+  }
+  .x-layout__sub-header-content {
+    min-height: 0;
+    transition: visibility 0.5s;
+    visibility: hidden;
+  }
+
+  .x-layout--is-scrolling-up {
+    .x-layout__sub-header {
+      grid-template-rows: 1fr;
+    }
+  }
+  .x-layout--is-scrolling-up {
+    .x-layout__sub-header .x-layout__sub-header-content {
+      visibility: visible;
+    }
   }
 </style>
