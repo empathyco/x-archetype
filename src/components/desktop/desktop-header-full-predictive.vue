1e9f821343d52ba3835f8a734109f1612a3d126a
Guillermo Cacheda <cachedacodes@gmail.com>
2023-06-06 10:39:26 +0200

    feat: create a full width empathize component (#286)
    EMP-741

---
+++ src/components/desktop/desktop-header-full-predictive.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="x-relative x-pb-24">
+    <MaxDesktopWidthItem>
+      <header class="x-flex x-items-center x-gap-48 x-pt-24">
+        <Logo />
+
+        <div class="x-flex x-flex-1 x-flex-col x-gap-16">
+          <div class="x-relative">
+            <SearchBox />
+          </div>
+        </div>
+
+        <CloseMainModal class="x-button-lead x-button-circle x-button-ghost x-justify-self-end">
+          <CrossIcon class="x-icon-lg" />
+        </CloseMainModal>
+      </header>
+    </MaxDesktopWidthItem>
+
+    <LocationProvider location="predictive_layer">
+      <FullWidthPredictive />
+    </LocationProvider>
+
+    <MaxDesktopWidthItem>
+      <DesktopSearchboxAlign class="x-layout-container">
+        <div class="x-layout-item">
+          <LocationProvider location="predictive_layer">
+            <RelatedTags v-if="$x.relatedTags.length > 0" class="x-pt-8" />
+          </LocationProvider>
+        </div>
+      </DesktopSearchboxAlign>
+    </MaxDesktopWidthItem>
+  </div>
+</template>
+
+<script lang="ts">
+  import { CloseMainModal, CrossIcon, LocationProvider } from '@empathyco/x-components';
+  import { defineComponent } from 'vue';
+  import SearchBox from '../search-box.vue';
+  import Logo from '../logo.vue';
+  import FullWidthPredictive from '../predictive-layer/full-width-predictive.vue';
+  import MaxDesktopWidthItem from '../max-desktop-width-item.vue';
+  import { RelatedTags } from '../search';
+  import DesktopSearchboxAlign from './desktop-searchbox-align.vue';
+
+  export default defineComponent({
+    components: {
+      DesktopSearchboxAlign,
+      MaxDesktopWidthItem,
+      FullWidthPredictive,
+      CloseMainModal,
+      CrossIcon,
+      Logo,
+      SearchBox,
+      RelatedTags,
+      LocationProvider
+    }
+  });
+</script>
---
+++ src/components/predictive-layer/predictive-next-queries.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="x-flex x-flex-col x-gap-4">
+    <h1 class="x-title4 x-title4-sm x-py-8 x-uppercase desktop:x-p-0">
+      {{ $t('nextQueries.title') }}
+    </h1>
+    <NextQueries
+      :animation="animation"
+      :max-items-to-render="3"
+      class="x-flex x-flex-col x-gap-8 desktop:x-gap-4"
+    >
+      <template #suggestion="{ suggestion }">
+        <NextQuery
+          class="x-suggestion-lg x-suggestion desktop:x-suggestion-md"
+          :suggestion="suggestion"
+        >
+          <CuratedCheckIcon v-if="suggestion.isCurated" class="x-icon-lg desktop:x-icon-md" />
+          <LightBulbOn v-else class="x-icon-lg desktop:x-icon-md" />
+          <span>{{ suggestion.query }}</span>
+        </NextQuery>
+      </template>
+    </NextQueries>
+  </div>
+</template>
+
+<script lang="ts">
+  import { AnimationProp, CuratedCheckIcon, LightBulbOn } from '@empathyco/x-components';
+  import { NextQueries, NextQuery } from '@empathyco/x-components/next-queries';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: {
+      CuratedCheckIcon,
+      LightBulbOn,
+      NextQuery,
+      NextQueries
+    },
+    props: {
+      animation: {
+        type: AnimationProp,
+        required: true
+      }
+    }
+  });
+</script>
+<style lang="scss"></style>
