ad8dcbbd7751db5878978b95aa8b9e0b2936434a
Laura Martinez Garcia <lauram@empathy.co>
2023-06-07 11:43:02 +0200

    separate animation from components

---
+++ src/components/is-scrolling-up.mixin.ts
@@ -0,0 +1,33 @@
+import { ScrollDirection, XOn } from '@empathyco/x-components';
+import Vue from 'vue';
+import Component from 'vue-class-component';
+@Component
+export default class IsScrollingUp extends Vue {
+  protected stopScrollDown = true;
+  protected scroll = true;
+  protected scrollOffset = 200;
+
+  @XOn('UserChangedScrollDirection')
+  scrollDirectionChanged(direction: ScrollDirection): void {
+    this.stopScrollDown = direction === 'UP';
+  }
+
+  @XOn(['UserScrolled'])
+  scrollLength(position: number): void {
+    if (this.stopScrollDown || position < this.scrollOffset) {
+      this.scroll = true;
+    } else if (!this.stopScrollDown && position > this.scrollOffset) {
+      this.scroll = false;
+    }
+  }
+
+  /**
+   * Checks the direction and the position of the scroll.
+   *
+   * @returns True if the user is scrolling up and has scrolled more than
+   * the defined scrollOffset.
+   */
+  protected get hasScrolled(): boolean {
+    return this.scroll;
+  }
+}
