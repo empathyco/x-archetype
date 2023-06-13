979ff84246a2bed84a889fe712df5736d3a8c37c
lauramargar <114984466+lauramargar@users.noreply.github.com>
2023-06-13 15:18:17 +0200

    feat: collapse desktop header in scrolling (#288)
    EMP-1003
    
    ---------
    
    Co-authored-by: Guillermo Cacheda <cachedacodes@gmail.com>

---
+++ src/components/collapse-height-animation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="x-collapse-height"
+    :class="{
+      'x-collapse-height--is-collapsed': isCollapsed
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    props: {
+      isCollapsed: {
+        type: Boolean
+      }
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  .x-collapse-height {
+    display: grid;
+    grid-template-rows: 1fr;
+    overflow: hidden;
+    transition: grid-template-rows 0.35s;
+
+    & > * {
+      min-height: 0;
+      transition: visibility 0.35s;
+      visibility: visible;
+    }
+
+    &--is-collapsed {
+      grid-template-rows: 0fr;
+      & > * {
+        visibility: hidden;
+      }
+    }
+  }
+</style>
--- src/components/desktop/desktop-header-full-predictive.vue
+++ src/components/desktop/desktop-header-full-predictive.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="x-relative x-pb-24">
+  <div class="x-relative x-pb-8">
     <MaxDesktopWidthItem>
       <header class="x-flex x-items-center x-gap-48 x-pt-24">
         <Logo />
@@ -19,16 +19,6 @@
     <LocationProvider location="predictive_layer">
       <FullWidthPredictive />
     </LocationProvider>
-
-    <MaxDesktopWidthItem>
-      <DesktopSearchboxAlign class="x-layout-container">
-        <div class="x-layout-item">
-          <LocationProvider location="predictive_layer">
-            <RelatedTags v-if="$x.relatedTags.length > 0" class="x-pt-8" />
-          </LocationProvider>
-        </div>
-      </DesktopSearchboxAlign>
-    </MaxDesktopWidthItem>
   </div>
 </template>
 
@@ -39,19 +29,15 @@
   import Logo from '../logo.vue';
   import FullWidthPredictive from '../predictive-layer/full-width-predictive.vue';
   import MaxDesktopWidthItem from '../max-desktop-width-item.vue';
-  import { RelatedTags } from '../search';
-  import DesktopSearchboxAlign from './desktop-searchbox-align.vue';
 
   export default defineComponent({
     components: {
-      DesktopSearchboxAlign,
       MaxDesktopWidthItem,
       FullWidthPredictive,
       CloseMainModal,
       CrossIcon,
       Logo,
       SearchBox,
-      RelatedTags,
       LocationProvider
     }
   });
