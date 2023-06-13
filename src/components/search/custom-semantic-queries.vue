d5160306230af9737734112a36ef2e14e65e7960
Abraham Javier Pérez Bautista <ajperezbau@protonmail.com>
2023-06-13 16:26:58 +0100

    feat: use SemanticQuery component
    EMP-1084

--- src/components/search/custom-semantic-queries.vue
+++ src/components/search/custom-semantic-queries.vue
@@ -1,5 +1,5 @@
 <template>
-  <SemanticQueries #default="{ queries }">
+  <SemanticQueries #default="{ queries, findSemanticQuery }">
     <section>
       <h1 class="x-title x-title1-md x-my-64 x-text-center">
         {{ $t('semanticQueries.title') }}
@@ -11,14 +11,14 @@
       >
         <CustomSlidingPanel>
           <template #header>
-            <BaseEventButton
-              :events="getEvent(query)"
+            <SemanticQuery
+              :suggestion="findSemanticQuery(query)"
               class="x-button-neutral x-button-tight x-button max-desktop:x-px-16"
             >
               {{ query }}
               ({{ totalResults }})
               <ArrowRightIcon class="x-icon-lg" />
-            </BaseEventButton>
+            </SemanticQuery>
           </template>
           <div class="x-flex x-gap-16 x-pt-16 max-desktop:x-px-16">
             <Result
@@ -35,32 +35,21 @@
 </template>
 
 <script lang="ts">
-  import { ArrowRightIcon, BaseEventButton, XEventsTypes } from '@empathyco/x-components';
+  import { ArrowRightIcon } from '@empathyco/x-components';
   import { defineComponent } from 'vue';
-  import { SemanticQueries } from '@empathyco/x-components/semantic-queries';
+  import { SemanticQueries, SemanticQuery } from '@empathyco/x-components/semantic-queries';
   import { QueryPreviewList } from '@empathyco/x-components/queries-preview';
   import CustomSlidingPanel from '../custom-sliding-panel.vue';
   import Result from '../results/result.vue';
 
   export default defineComponent({
     components: {
-      BaseEventButton,
       ArrowRightIcon,
       CustomSlidingPanel,
       QueryPreviewList,
       Result,
-      SemanticQueries
-    },
-    setup() {
-      const getEvent = (query: string): Partial<XEventsTypes> => {
-        return {
-          UserAcceptedAQuery: query
-        };
-      };
-
-      return {
-        getEvent
-      };
+      SemanticQueries,
+      SemanticQuery
     }
   });
 </script>
