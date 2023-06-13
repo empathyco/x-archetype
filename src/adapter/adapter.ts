122085acda6d45aabbc69ffe36f6bbecf3c191c7
herrardo <gerardov@empathy.co>
2023-06-13 13:31:13 +0200

    feat: enable tests and set semantics filter

--- src/adapter/adapter.ts
+++ src/adapter/adapter.ts
@@ -49,7 +49,7 @@ semanticQueriesRequestSchema.$override<
   extraParams: ({ extraParams }) => {
     return {
       extraParams,
-      filter_ids: 'NOT_PARTIAL%2CNOT_ANY_WORDS'
+      filter_ids: 'NOT_PARTIAL%2CNOT_ALL_WORDS'
     };
   }
 });
