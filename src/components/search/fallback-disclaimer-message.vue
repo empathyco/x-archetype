86bb9da357467d9dc19cf5ae247166435d82dd51
Guillermo <cachedacodes@gmail.com>
2023-10-02 11:39:54 +0200

    chore: add v-if to fallback-disclamer-message

--- src/components/search/fallback-disclaimer-message.vue
+++ src/components/search/fallback-disclaimer-message.vue
@@ -1,5 +1,5 @@
 <template>
-  <FallbackDisclaimer class="x-fallback-message x-message">
+  <FallbackDisclaimer v-if="$x.fromNoResultsWithFilters" class="x-fallback-message x-message">
     <i18n class="x-text1 desktop:x-text1-lg" path="fallbackDisclaimer.message" tag="p">
       <template #query>
         <span class="x-w-auto x-font-bold">"{{ $x.query.search }}"</span>
