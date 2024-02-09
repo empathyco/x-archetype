5b908397d948b20c74903a5af0dbf7ce70b5a226
Diego Pascual <diegopf@users.noreply.github.com>
2024-02-09 09:05:08 +0100

    undo custom event emit

--- src/shims-x-components.d.ts
+++ src/shims-x-components.d.ts
@@ -3,6 +3,5 @@ import { SemanticQueriesConfig } from '@empathyco/x-components/semantic-queries'
 declare module '@empathyco/x-components' {
   interface XEventsTypes {
     SemanticQueriesConfigProvided: SemanticQueriesConfig;
-    UserAcceptedAWysiwygLogin: void;
   }
 }
