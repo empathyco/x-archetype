562b610751241da8398a8554277d317dedd73df6
Laura Martinez Garcia <lauram@empathy.co>
2024-02-26 15:49:21 +0100

    fix: open empathize with autoFocus true

--- src/components/predictive-layer/predictive-layer.vue
+++ src/components/predictive-layer/predictive-layer.vue
@@ -3,117 +3,119 @@
     :animation="empathizeAnimation"
     class="x-bg-neutral-0 desktop:x-z-[1] desktop:x-w-full"
   >
-    <component
-      :is="isDesktopOrGreater ? 'div' : 'BaseScroll'"
-      v-if="showEmpathize || showIdentifierResults"
-      class="x-h-full desktop:x-my-16 desktop:x-ml-16"
-    >
-      <div class="x-layout-item desktop:x-block">
-        <BaseKeyboardNavigation
-          class="x-flex x-flex-col desktop:x-grid desktop:x-grid-cols-12 desktop:x-items-start desktop:x-gap-24"
-          :navigationHijacker="navigationHijacker"
-        >
-          <PredictiveIdentifierResults
-            v-if="showIdentifierResults"
-            :animation="suggestionsAnimation"
-          />
-
-          <div
-            v-else-if="showEmpathize"
-            class="x-flex x-flex-col x-gap-8 x-pl-8 desktop:x-col-span-5 desktop:x-pl-0"
-            :class="$x.query.searchBox ? 'desktop:x-gap-4' : 'desktop:x-gap-16'"
+    <div>
+      <component
+        :is="isDesktopOrGreater ? 'div' : 'BaseScroll'"
+        v-if="showEmpathize || showIdentifierResults"
+        class="x-h-full desktop:x-my-16 desktop:x-ml-16"
+      >
+        <div class="x-layout-item desktop:x-block">
+          <BaseKeyboardNavigation
+            class="x-flex x-flex-col desktop:x-grid desktop:x-grid-cols-12 desktop:x-items-start desktop:x-gap-24"
+            :navigationHijacker="navigationHijacker"
           >
-            <BaseIdModalOpen
-              v-if="isTabletOrLess && !$x.query.searchBox"
-              modalId="my-history-aside"
-              class="x-button-neutral x-button-sm x-button-tight x-self-end x-pr-8"
+            <PredictiveIdentifierResults
+              v-if="showIdentifierResults"
+              :animation="suggestionsAnimation"
+            />
+
+            <div
+              v-else-if="showEmpathize"
+              class="x-flex x-flex-col x-gap-8 x-pl-8 desktop:x-col-span-5 desktop:x-pl-0"
+              :class="$x.query.searchBox ? 'desktop:x-gap-4' : 'desktop:x-gap-16'"
             >
-              {{ $t('myHistory.openButton') }}
-              <SettingsIcon class="x-icon-lg" />
-            </BaseIdModalOpen>
-            <div v-if="showHistoryQueries" class="x-flex x-flex-col x-gap-4">
-              <div v-if="!$x.query.searchBox" class="x-flex x-items-center">
-                <h1 class="x-title4 x-title4-sm x-flex-1 x-uppercase">
-                  {{ $t('historyQueries.title') }}
-                </h1>
-                <ClearHistoryQueries class="x-button-neutral x-button-sm x-button-tight x-px-8">
-                  <TrashIcon v-if="isTabletOrLess" class="x-icon-lg" />
-                  <span v-else>{{ $t('historyQueries.clear') }}</span>
-                </ClearHistoryQueries>
+              <BaseIdModalOpen
+                v-if="isTabletOrLess && !$x.query.searchBox"
+                modalId="my-history-aside"
+                class="x-button-neutral x-button-sm x-button-tight x-self-end x-pr-8"
+              >
+                {{ $t('myHistory.openButton') }}
+                <SettingsIcon class="x-icon-lg" />
+              </BaseIdModalOpen>
+              <div v-if="showHistoryQueries" class="x-flex x-flex-col x-gap-4">
+                <div v-if="!$x.query.searchBox" class="x-flex x-items-center">
+                  <h1 class="x-title4 x-title4-sm x-flex-1 x-uppercase">
+                    {{ $t('historyQueries.title') }}
+                  </h1>
+                  <ClearHistoryQueries class="x-button-neutral x-button-sm x-button-tight x-px-8">
+                    <TrashIcon v-if="isTabletOrLess" class="x-icon-lg" />
+                    <span v-else>{{ $t('historyQueries.clear') }}</span>
+                  </ClearHistoryQueries>
+                </div>
+
+                <HistoryQueries
+                  :animation="suggestionsAnimation"
+                  :max-items-to-render="$x.query.searchBox ? 2 : 4"
+                  suggestionItemClass="x-w-full"
+                  class="-x-mr-2 x-flex x-flex-col x-gap-8 desktop:x-gap-4"
+                >
+                  <template #suggestion="{ suggestion }">
+                    <HistoryQuery
+                      class="x-suggestion-group-lg desktop:x-suggestion-group-md hover:x-no-underline"
+                      :suggestion="suggestion"
+                      suggestionClass="x-suggestion x-suggestion-lg desktop:x-suggestion-md"
+                    >
+                      <template #default="{ query }">
+                        <HistoryIcon class="x-icon-lg desktop:x-icon-md" />
+                        <div class="x-group x-flex x-flex-col x-gap-2">
+                          <Highlight
+                            :text="suggestion.query"
+                            :highlight="query"
+                            class="group-hover:x-underline"
+                          />
+                          <HistoryQueryFilters
+                            class="x-w-192 desktop:x-w-128"
+                            :filtersList="suggestion.selectedFilters"
+                          />
+                        </div>
+                      </template>
+
+                      <template #remove-button-content>
+                        <span
+                          :aria-label="
+                            $t('historyQueries.removeLabel', { suggestion: suggestion.query })
+                          "
+                        >
+                          <CrossTinyIcon class="x-icon-lg desktop:x-icon-md" />
+                        </span>
+                      </template>
+                    </HistoryQuery>
+                  </template>
+                </HistoryQueries>
               </div>
 
-              <HistoryQueries
+              <PredictiveQuerySuggestions
+                v-if="showQuerySuggestions"
                 :animation="suggestionsAnimation"
-                :max-items-to-render="$x.query.searchBox ? 2 : 4"
-                suggestionItemClass="x-w-full"
-                class="-x-mr-2 x-flex x-flex-col x-gap-8 desktop:x-gap-4"
-              >
-                <template #suggestion="{ suggestion }">
-                  <HistoryQuery
-                    class="x-suggestion-group-lg hover:x-no-underline desktop:x-suggestion-group-md"
-                    :suggestion="suggestion"
-                    suggestionClass="x-suggestion x-suggestion-lg desktop:x-suggestion-md"
-                  >
-                    <template #default="{ query }">
-                      <HistoryIcon class="x-icon-lg desktop:x-icon-md" />
-                      <div class="x-group x-flex x-flex-col x-gap-2">
-                        <Highlight
-                          :text="suggestion.query"
-                          :highlight="query"
-                          class="group-hover:x-underline"
-                        />
-                        <HistoryQueryFilters
-                          class="x-w-192 desktop:x-w-128"
-                          :filtersList="suggestion.selectedFilters"
-                        />
-                      </div>
-                    </template>
+              />
 
-                    <template #remove-button-content>
-                      <span
-                        :aria-label="
-                          $t('historyQueries.removeLabel', { suggestion: suggestion.query })
-                        "
-                      >
-                        <CrossTinyIcon class="x-icon-lg desktop:x-icon-md" />
-                      </span>
-                    </template>
-                  </HistoryQuery>
-                </template>
-              </HistoryQueries>
+              <PredictiveNextQueries v-if="showNextQueries" :animation="suggestionsAnimation" />
+
+              <PredictivePopularSearches
+                v-if="showPopularSearches"
+                :animation="suggestionsAnimation"
+              />
+
+              <BaseIdModalOpen
+                v-if="isDesktopOrGreater && !$x.query.searchBox"
+                modalId="my-history-aside"
+                class="x-button-neutral x-button-sm x-button-tight x-self-start"
+                data-test="my-history-button"
+              >
+                <SettingsIcon />
+                {{ $t('myHistory.openButton') }}
+              </BaseIdModalOpen>
             </div>
 
-            <PredictiveQuerySuggestions
-              v-if="showQuerySuggestions"
-              :animation="suggestionsAnimation"
-            />
-
-            <PredictiveNextQueries v-if="showNextQueries" :animation="suggestionsAnimation" />
-
-            <PredictivePopularSearches
-              v-if="showPopularSearches"
-              :animation="suggestionsAnimation"
-            />
-
-            <BaseIdModalOpen
+            <SlidingRecommendations
               v-if="isDesktopOrGreater && !$x.query.searchBox"
-              modalId="my-history-aside"
-              class="x-button-neutral x-button-sm x-button-tight x-self-start"
-              data-test="my-history-button"
-            >
-              <SettingsIcon />
-              {{ $t('myHistory.openButton') }}
-            </BaseIdModalOpen>
-          </div>
-
-          <SlidingRecommendations
-            v-if="isDesktopOrGreater && !$x.query.searchBox"
-            class="x-col-span-7"
-          />
-        </BaseKeyboardNavigation>
-      </div>
-      <SlidingRecommendations v-if="isTabletOrLess && !$x.query.searchBox" />
-    </component>
+              class="x-col-span-7"
+            />
+          </BaseKeyboardNavigation>
+        </div>
+        <SlidingRecommendations v-if="isTabletOrLess && !$x.query.searchBox" />
+      </component>
+    </div>
   </Empathize>
 </template>
 
