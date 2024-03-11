028ba3aec9915f7e07056ad85a4b221420ef8520
acondal <annac@empathy.co>
2024-03-11 15:28:01 +0100

    fix: set autofocus false if no QP
    EMP-3650

--- src/components/custom-main-modal.vue
+++ src/components/custom-main-modal.vue
@@ -1,13 +1,17 @@
 <template>
-  <MainModal :animation="animation" :class="`x-${deviceName}`" :focusOnOpen="isTabletOrLess">
+  <MainModal
+    :animation="animation"
+    :class="`x-${deviceName}`"
+    :focusOnOpen="isTabletOrLess && focusOnOpen"
+  >
     <Mobile v-if="isTabletOrLess" />
     <Desktop v-else />
   </MainModal>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { animateClipPath, MainModal } from '@empathyco/x-components';
+  import { defineComponent, inject } from 'vue';
+  import { animateClipPath, MainModal, SnippetConfig } from '@empathyco/x-components';
   import { useDevice } from '../composables/use-device.composable';
   import Mobile from './mobile/mobile.vue';
   import Desktop from './desktop/desktop.vue';
@@ -20,10 +24,21 @@
     },
     setup() {
       const { isTabletOrLess, deviceName } = useDevice();
+      const snippetConfig = <SnippetConfig>inject('snippetConfig');
+
+      /**
+       * Sets focusOnOpen to false if there are no query previews
+       * to avoid having to click the input manually to be able to write.
+       */
+      const focusOnOpen = !(
+        !!snippetConfig.queriesPreview && snippetConfig.queriesPreview.length > 0
+      );
+
       return {
         animation: animateClipPath(),
         deviceName,
-        isTabletOrLess
+        isTabletOrLess,
+        focusOnOpen
       };
     }
   });
