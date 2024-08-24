c458051257dd471ffeaeee6d7ebfa995c036716d
Diego Pascual <diegopf@users.noreply.github.com>
2024-08-24 17:00:40 +0200

    initial vite config

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import Inspector from 'vite-plugin-vue-inspector';
+import injectHTML from 'vite-plugin-html-inject';
+
+export const vueDocsPlugin = {
+  name: 'vue-docs',
+  transform(code: string, id: string) {
+    return !/vue&type=docs/.test(id) ? undefined : `export default ''`;
+  }
+};
+
+export default defineConfig({
+  plugins: [injectHTML(), vue(), vueDocsPlugin, Inspector()],
+  server: {
+    port: 8080
+  },
+  preview: {
+    port: 8080
+  },
+  optimizeDeps: {
+    exclude: ['@empathyco/x-components']
+  }
+});
