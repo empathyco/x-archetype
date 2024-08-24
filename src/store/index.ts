9653a45587db92a6f540dbc7cfef01c4e4e59060
Diego Pascual <diegopf@users.noreply.github.com>
2024-08-24 22:08:15 +0200

    avoid strict mode for vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,7 @@
-import Vue from 'vue';
-import Vuex, { Store } from 'vuex';
+import { createStore } from 'vuex';
 
-Vue.use(Vuex);
-
-export default new Store({
-  strict: process.env.NODE_ENV !== 'production',
+export default createStore({
+  strict: false,
   state: {},
   mutations: {},
   actions: {},
