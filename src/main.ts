bcb47064fb9e0d4f98af1b0fc428a05e15acbbc4
Diego Pascual <diegopf@users.noreply.github.com>
2024-08-28 17:35:29 +0200

    get rid of obsolete config apis

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { CssInjector } from '@empathyco/x-archetype-utils';
 import { XInstaller } from '@empathyco/x-components';
-import Vue from 'vue';
 import { getInstallXOptions } from './x-components/plugin.options';
 
 declare global {
@@ -20,9 +19,6 @@ declare global {
     };
   }
 }
-
-Vue.config.productionTip = false;
-Vue.config.devtools = window.__enableVueDevtools__ ?? false;
 
 new CssInjector(true);
 getInstallXOptions().then(installXOptions => new XInstaller(installXOptions).init());
