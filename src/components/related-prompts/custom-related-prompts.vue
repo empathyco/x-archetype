3c187479701507d88d9b142a0e50b0eacff1fb3a
lauramargar <114984466+lauramargar@users.noreply.github.com>
2025-02-20 15:20:33 +0100

    feat: add related-prompts tagging (#535)

--- src/components/related-prompts/custom-related-prompts.vue
+++ src/components/related-prompts/custom-related-prompts.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="x-mb-64 x-flex x-flex-col">
     <i18n-t
-      class="x-no-results-message x-text1 x-flex x-flex-wrap x-gap-4 x-items-center x-justify-center x-break-words x-bg-neutral-10 x-p-24 x-text-center x-text-neutral-90 desktop:x-text1-lg"
+      class="x-no-results-message x-text1 x-mb-24 x-flex x-flex-wrap x-gap-4 x-items-center x-justify-center x-break-words x-bg-neutral-10 x-p-24 x-text-center x-text-neutral-90 desktop:x-text1-lg"
       keypath="relatedPrompts.title"
       tag="p"
       scope="global"
@@ -15,32 +15,33 @@
     </i18n-t>
 
     <div class="x-flex x-flex-col">
-      <RelatedPromptsTagList :class="isDesktopOrGreater ? 'x-mt-24' : 'x-mt-16'" />
+      <RelatedPrompts :class="isDesktopOrGreater ? 'x-mt-24' : 'x-mt-16'" />
       <CustomQueryPreview
         v-if="selectedPrompt !== -1"
         :key="queriesPreviewInfo.length"
         class="x-rounded-b-[12px] x-bg-neutral-10 x-px-16"
         :queries-preview-info="queriesPreviewInfo"
+        query-feature="related_prompts"
       ></CustomQueryPreview>
     </div>
   </div>
 </template>
 <script lang="ts">
-import type { RelatedPrompt } from '@empathyco/x-types'
+import type { RelatedPrompt, RelatedPromptNextQuery } from '@empathyco/x-types'
 import type { PropType } from 'vue'
 import { use$x, useState } from '@empathyco/x-components'
 import { relatedPromptsXModule } from '@empathyco/x-components/related-prompts'
 import { computed, defineComponent } from 'vue'
 import { useDevice } from '../../composables/use-device.composable'
 import CustomQueryPreview from '../search/results/custom-query-preview.vue'
-import RelatedPromptsTagList from './related-prompts-tag-list.vue'
+import RelatedPrompts from './related-prompts.vue'
 
 export default defineComponent({
   name: 'CustomRelatedPrompts',
   xModule: relatedPromptsXModule.name,
   components: {
+    RelatedPrompts,
     CustomQueryPreview,
-    RelatedPromptsTagList,
   },
   props: {
     relatedPromptList: {
@@ -52,19 +53,20 @@ export default defineComponent({
   setup() {
     const x = use$x()
     const { isDesktopOrGreater } = useDevice()
-    const { relatedPrompts, selectedPrompt, selectedQuery } = useState('relatedPrompts', [
+    const { relatedPrompts, selectedPrompt } = useState('relatedPrompts', [
       'relatedPrompts',
       'selectedPrompt',
-      'selectedQuery',
     ])
 
     const queriesPreviewInfo = computed(() => {
-      const queries = relatedPrompts.value[selectedPrompt.value].nextQueries as string[]
-      if (selectedQuery.value === -1) {
+      if (relatedPrompts.value.length) {
+        const queries = [] as string[]
+        relatedPrompts.value[selectedPrompt.value].relatedPromptNextQueries.forEach(
+          (nextQuery: RelatedPromptNextQuery) => queries.push(nextQuery.query),
+        )
         return queries.map(query => ({ query }))
-      } else {
-        return [{ query: queries[selectedQuery.value] }]
       }
+      return []
     })
 
     return {
