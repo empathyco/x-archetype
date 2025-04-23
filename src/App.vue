24f72bf185133774c48fa5101b0086d14873209f
Laura Martinez Garcia <lauram@empathy.co>
2025-04-23 11:46:20 +0200

    chore: update dependencies

--- src/App.vue
+++ src/App.vue
@@ -50,15 +50,12 @@ export default defineComponent({
     const x = use$x()
     const { isDesktopOrGreater } = useDevice()
     const snippetConfig = inject<SnippetConfig>('snippetConfig')!
-    const { relatedPrompts, selectedPrompt } = useState('relatedPrompts', [
-      'relatedPrompts',
-      'selectedPrompt',
-    ])
+    const { relatedPrompts, selectedPrompt } = useState('relatedPrompts')
 
     const queriesPreviewInfo = computed(() => {
       if (relatedPrompts.value.length) {
         const queries = [] as string[]
-        relatedPrompts.value[selectedPrompt.value].relatedPromptNextQueries.forEach(
+        relatedPrompts.value[selectedPrompt.value].relatedPromptNextQueries?.forEach(
           (nextQuery: RelatedPromptNextQuery) => queries.push(nextQuery.query),
         )
         return queries.map(query => ({ query }))
