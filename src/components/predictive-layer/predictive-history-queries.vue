0e9cc655a51990eaf178a19b926febf13e0c5fcd
Víctor CG <victorcg@empathy.co>
2026-03-11 12:05:00 +0100

    feat: implement horizontal layout for predictive suggestions

--- src/components/predictive-layer/predictive-history-queries.vue
+++ src/components/predictive-layer/predictive-history-queries.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="x-flex x-flex-col x-gap-4">
-    <div v-if="!x.query.searchBox" class="x-flex x-items-center">
+    <div v-if="!x.query.searchBox" class="x-flex x-min-h-32 x-items-center">
       <h1 class="x-title4 x-title4-sm x-flex-1 x-uppercase">
         {{ $t('historyQueries.title') }}
       </h1>
@@ -10,41 +10,54 @@
       </ClearHistoryQueries>
     </div>
 
-    <HistoryQueries
-      :animation="suggestionsAnimation"
-      :max-items-to-render="x.query.searchBox ? 2 : 4"
-      suggestion-item-class="x-w-full"
-      class="-x-mr-2 x-flex x-flex-col x-gap-8 desktop:x-gap-4"
+    <component
+      :is="horizontalLayout ? SlidingPanel : 'div'"
+      :show-buttons="false"
+      :reset-on-content-change="false"
+      button-class="x-button-lead x-button-circle x-button-ghost x-p-0"
     >
-      <template #suggestion="{ suggestion }">
-        <HistoryQuery
-          class="x-suggestion-group-lg desktop:x-suggestion-group-md hover:x-no-underline"
-          :suggestion="suggestion"
-          suggestion-class="x-suggestion x-suggestion-lg desktop:x-suggestion-md"
-        >
-          <template #default="{ query }">
-            <HistoryIcon class="x-icon-lg desktop:x-icon-md" />
-            <div class="x-group x-flex x-flex-col x-gap-2">
-              <Highlight
-                :text="suggestion.query"
-                :highlight="query"
-                class="group-hover:x-underline"
-              />
-              <HistoryQueryFilters
-                class="x-w-192 desktop:x-w-128"
-                :filters-list="suggestion.selectedFilters"
-              />
-            </div>
-          </template>
+      <HistoryQueries
+        :animation="suggestionsAnimation"
+        :max-items-to-render="horizontalLayout ? 5 : 2"
+        class="x-gap-8 desktop:x-gap-4"
+        :class="{ 'x-suggestions--horizontal': horizontalLayout }"
+      >
+        <template #suggestion="{ suggestion }">
+          <HistoryQuery
+            class="hover:x-no-underline"
+            :class="{
+              'x-suggestion-group-outlined': horizontalLayout,
+              'x-suggestion-group-lg desktop:x-suggestion-group-md': !horizontalLayout,
+            }"
+            :suggestion="suggestion"
+            :suggestion-class="`x-suggestion  ${!horizontalLayout ? 'x-suggestion-lg desktop:x-suggestion-md' : ''}`"
+          >
+            <template #default="{ query }">
+              <HistoryIcon class="x-icon-lg x-mb-4 desktop:x-icon-md desktop:x-mb-0" />
+              <div class="x-flex x-flex-col x-gap-2">
+                <Highlight
+                  :text="suggestion.query"
+                  :highlight="query"
+                  class="group-hover:x-no-underline"
+                />
+                <HistoryQueryFilters
+                  class="x-w-192 desktop:x-w-128"
+                  :filters-list="suggestion.selectedFilters"
+                />
+              </div>
+            </template>
 
-          <template #remove-button-content>
-            <span :aria-label="$t('historyQueries.removeLabel', { suggestion: suggestion.query })">
-              <CrossTinyIcon class="x-icon-lg desktop:x-icon-md" />
-            </span>
-          </template>
-        </HistoryQuery>
-      </template>
-    </HistoryQueries>
+            <template #remove-button-content>
+              <span
+                :aria-label="$t('historyQueries.removeLabel', { suggestion: suggestion.query })"
+              >
+                <CrossTinyIcon class="x-icon-lg desktop:x-icon-md" />
+              </span>
+            </template>
+          </HistoryQuery>
+        </template>
+      </HistoryQueries>
+    </component>
   </div>
 </template>
 <script setup lang="ts">
@@ -52,6 +65,7 @@ import {
   CrossTinyIcon,
   Highlight,
   HistoryIcon,
+  SlidingPanel,
   StaggeredFadeAndSlide,
   TrashIcon,
   use$x,
@@ -61,10 +75,13 @@ import {
   HistoryQueries,
   HistoryQuery,
 } from '@empathyco/x-components/history-queries'
+import { computed } from 'vue'
 import { useDevice } from '../../composables/use-device.composable'
 import HistoryQueryFilters from '../history-query-filters.vue'
 
 const suggestionsAnimation = StaggeredFadeAndSlide as any
 const { isTabletOrLess } = useDevice()
 const x = use$x()
+
+const horizontalLayout = computed(() => isTabletOrLess.value && !x.query.searchBox)
 </script>
--- src/components/predictive-layer/predictive-popular-searches.vue
+++ src/components/predictive-layer/predictive-popular-searches.vue
@@ -1,33 +1,53 @@
 <template>
   <div class="x-mt-8 x-flex x-flex-col x-gap-4 desktop:x-mt-0">
-    <h1 class="x-title4 x-title4-sm x-uppercase">
-      {{ $t('popularSearches.title') }}
-    </h1>
-    <PopularSearches
-      :animation="animation"
-      :max-items-to-render="4"
-      class="x-flex x-flex-col x-gap-8 desktop:x-gap-4"
+    <div class="x-flex x-min-h-32 x-items-center">
+      <h1 class="x-title4 x-title4-sm x-uppercase">
+        {{ $t('popularSearches.title') }}
+      </h1>
+    </div>
+    <component
+      :is="horizontalLayout ? SlidingPanel : 'div'"
+      :show-buttons="false"
+      :reset-on-content-change="false"
+      button-class="x-button-lead x-button-circle x-button-ghost x-p-0"
     >
-      <template #suggestion="{ suggestion }">
-        <PopularSearch
-          class="x-suggestion x-suggestion-lg desktop:x-suggestion-md"
-          :suggestion="suggestion"
-        >
-          <TrendingIcon class="x-icon-lg desktop:x-icon-md" />
-          <span>{{ suggestion.query }}</span>
-        </PopularSearch>
-      </template>
-    </PopularSearches>
+      <PopularSearches
+        :animation="animation"
+        :max-items-to-render="horizontalLayout ? 5 : 4"
+        class="x-gap-8 desktop:x-gap-4"
+        :class="{ 'x-suggestions--horizontal': horizontalLayout }"
+      >
+        <template #suggestion="{ suggestion }">
+          <PopularSearch
+            class="x-suggestion"
+            :class="{
+              'x-suggestion-outlined x-gap-8': horizontalLayout,
+              'x-suggestion-lg desktop:x-suggestion-md': !horizontalLayout,
+            }"
+            :suggestion="suggestion"
+          >
+            <TrendingIcon class="x-icon-lg x-mb-4 desktop:x-icon-md desktop:x-mb-0" />
+            <span>{{ suggestion.query }}</span>
+          </PopularSearch>
+        </template>
+      </PopularSearches>
+    </component>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { AnimationProp } from '@empathyco/x-components'
-import { TrendingIcon } from '@empathyco/x-components'
+import { SlidingPanel, TrendingIcon, use$x } from '@empathyco/x-components'
 import { PopularSearch, PopularSearches } from '@empathyco/x-components/popular-searches'
+import { computed } from 'vue'
+import { useDevice } from '../../composables/use-device.composable'
 
 defineProps<{
   animation: typeof AnimationProp
 }>()
+
+const { isTabletOrLess } = useDevice()
+const x = use$x()
+
+const horizontalLayout = computed(() => isTabletOrLess.value && !x.query.searchBox)
 </script>
-<style lang="scss"></style>
