876c63b94687a733be9cafbc2240e22484debc1f
lauramargar <114984466+lauramargar@users.noreply.github.com>
2026-03-31 13:07:17 +0200

    feat: manage predictive layer with x controls (#739)

--- src/components/desktop/desktop-top-section.vue
+++ src/components/desktop/desktop-top-section.vue
@@ -1,20 +1,31 @@
 <template>
   <div>
-    <DesktopHeaderFullPredictive />
+    <DesktopHeaderFullPredictive v-if="isFullPredictiveLayer" />
+    <DesktopHeaderFloatingPredictive v-else />
 
     <DesktopSubHeader
       data-test="sub-header"
       :has-searched="hasSearched"
-      :is-full-predictive="true"
+      :is-full-predictive="isFullPredictiveLayer"
       class="x-layout-item"
     />
   </div>
 </template>
 
 <script setup lang="ts">
+import type { ComputedRef } from 'vue'
+import { defineAsyncComponent } from 'vue'
+import { useExperienceControls } from '../../composables/use-experience-controls.composable'
 import { useHasSearched } from '../../composables/use-has-searched.composable'
-import DesktopHeaderFullPredictive from './desktop-header-full-predictive.vue'
 import DesktopSubHeader from './desktop-sub-header.vue'
+const DesktopHeaderFloatingPredictive = defineAsyncComponent(() =>
+  import('./desktop-header-floating-predictive.vue').then(m => m.default),
+)
+const DesktopHeaderFullPredictive = defineAsyncComponent(() =>
+  import('./desktop-header-full-predictive.vue').then(m => m.default),
+)
 
 const { hasSearched } = useHasSearched()
+const { getControlFromPath } = useExperienceControls()
+const isFullPredictiveLayer = getControlFromPath('fullPredictiveLayer') as ComputedRef<boolean>
 </script>
