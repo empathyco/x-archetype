d612ae2c59793c3dcfad9387fe21056c473ff58e
Jose A. Cabaneros <joseacabaneros@gmail.com>
2026-04-09 17:32:11 +0200

    build: fix safari, tw and shadowroot issue

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,26 @@ function overrideXCssInjector(): PluginOption {
   }
 }
 
+// TODO - Tailwind 4 + ShadowRoot issue enclosing @supports block. Especially
+//  the `-webkit-hyphens: none` condition. `@supports ((-webkit-hyphens: none) and`
+//  To avoid issues with Chrome and Safari, we replace the whole `@supports` block.
+// https://github.com/tailwindlabs/tailwindcss/issues/15005#issuecomment-3722970702
+// https://github.com/tailwindlabs/tailwindcss/discussions/15556#discussioncomment-15063817
+function injectCssCode(cssCode: string) {
+  const normalizedCssCodeForShadowRoot = cssCode
+    // BUILD
+    .replace(
+      '(((-webkit-hyphens:none)) and (not (margin-trim:inline))) or ((-moz-orient:inline) and (not (color:rgb(from red r g b))))',
+      '(display: block)',
+    )
+    // DEV
+    .replace(
+      '((-webkit-hyphens: none) and (not (margin-trim: inline))) or ((-moz-orient: inline) and (not (color:rgb(from red r g b))))',
+      '(display: block)',
+    )
+  return window.xCSSInjector.addStyle({ source: normalizedCssCodeForShadowRoot })
+}
+
 export default defineConfig({
   build: {
     rollupOptions: {
@@ -53,17 +73,9 @@ export default defineConfig({
     vue(),
     tailwindcss(),
     cssInjectedByJsPlugin({
-      injectCodeFunction: cssCode => {
-        // TODO - Tailwind 4 + ShadowRoot issue enclosing @supports block. Especially
-        //  the `-webkit-hyphens: none` condition. `@supports ((-webkit-hyphens: none) and`
-        // https://github.com/tailwindlabs/tailwindcss/issues/15005#issuecomment-3722970702
-        const normalizedCssCodeForShadowRoot = cssCode
-          .replaceAll('((-webkit-hyphens:none)) and ', '') // BUILD
-          .replaceAll('(-webkit-hyphens: none) and ', '') // DEV
-        return window.xCSSInjector.addStyle({ source: normalizedCssCodeForShadowRoot })
-      },
       topExecutionPriority: false, // Wait until `window.xCSSInjector` is created.
       dev: { enableDev: true },
+      injectCodeFunction: injectCssCode,
     }),
     visualizer(),
   ],
