ba7d9cdb83b58bff80ecd1e73f99b4fd7548436e
Laura Martinez Garcia <lauram@empathy.co>
2026-04-27 16:05:50 +0200

    feat: implement theme to accept customization in runtime

--- src/composables/use-customization.composable.ts
+++ src/composables/use-customization.composable.ts
@@ -1,47 +1,17 @@
 import type { Dictionary } from '@empathyco/x-utils'
 import { useState } from '@empathyco/x-components'
-import { ref, watch } from 'vue'
 
-interface Customization {
-  variables?: Dictionary<string>
-}
-
-const objectToCss = (cssObject: Dictionary<string | number>) =>
-  Object.entries(cssObject)
-    .map(([cssVar, value]) => `${cssVar}:${value}`)
+function mapCustomizationStylesToCSS(customizationStyles: Dictionary<string>) {
+  return Object.entries(customizationStyles)
+    .map(([key, value]) => `--xds-${key}:${value}`)
     .join(';')
-
-const mapVariables = (variables: Dictionary<string>) => {
-  return {
-    '--xds-color-lead-25': variables.colorLead25,
-    '--xds-color-lead-50': variables.colorLead50,
-    '--xds-color-lead-75': variables.colorLead75,
-    '--xds-color-neutral-10': variables.colorNeutral10,
-    '--xds-color-neutral-25': variables.colorNeutral25,
-    '--xds-color-neutral-50': variables.colorNeutral50,
-    '--xds-color-neutral-75': variables.colorNeutral75,
-    '--xds-color-neutral-90': variables.colorNeutral90,
-    '--xds-font-family-main': variables.fontFamilyMain,
-    '--xds-font-weight-light': variables.fontWeightLight,
-    '--xds-font-weight-regular': variables.fontWeightRegular,
-    '--xds-font-weight-bold': variables.fontWeightBold,
-  }
 }
 
-export const useCustomization = () => {
-  const { params } = useState('search')
-  const jsonStyles = ref<Customization | null>(null)
+export function useCustomization() {
+  const { controls } = useState('experienceControls')
 
-  watch(
-    () => params.value.instance as string,
-    async instance => {
-      const styles = await fetch(`./src/styles/${instance}.json`)
-      jsonStyles.value = (await styles.json()) as Customization
-      if (jsonStyles.value && jsonStyles.value.variables) {
-        window.xCSSInjector.addStyle({
-          source: `:root,:host{${objectToCss(mapVariables(jsonStyles.value.variables))}}`,
-        })
-      }
-    },
-  )
+  if (controls.value.styles) {
+    const xdsStyles = mapCustomizationStylesToCSS(controls.value.styles as Dictionary<string>)
+    window.xCSSInjector.addStyle({ source: `:root,:host{${xdsStyles}}` })
+  }
 }
