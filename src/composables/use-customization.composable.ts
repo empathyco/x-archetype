cbda27b73858e0d5b320fb04cca77548bd85e488
lauramargar <114984466+lauramargar@users.noreply.github.com>
2026-04-28 12:16:48 +0200

    feat: implement theme to accept customization in runtime (#756)

---
+++ src/composables/use-customization.composable.ts
@@ -0,0 +1,21 @@
+import type { Dictionary } from '@empathyco/x-utils'
+import { useState } from '@empathyco/x-components'
+
+function mapCustomizationStylesToCSS(customizationStyles: Dictionary<string>) {
+  return Object.entries(customizationStyles)
+    .map(([key, value]) => `--xds-${key}:${value}`)
+    .join(';')
+}
+
+export function useCustomization() {
+  const init = () => {
+    const { controls } = useState('experienceControls')
+
+    if (controls.value.styles) {
+      const xdsStyles = mapCustomizationStylesToCSS(controls.value.styles as Dictionary<string>)
+      window.xCSSInjector.addStyle({ source: `:root,:host{${xdsStyles}}` })
+    }
+  }
+
+  return { init }
+}
--- src/x-components/xcontrols.ts
+++ src/x-components/xcontrols.ts
@@ -1,4 +1,18 @@
 export const xControlsState = {
   aiSearchFallback: true,
   fullPredictiveLayer: false,
+  styles: {
+    'color-lead-25': '#bbc9cf',
+    'color-lead-50': '#5e7782',
+    'color-lead-75': '#243d48',
+    'color-neutral-10': '#eef1f2',
+    'color-neutral-25': '#dbe2e5',
+    'color-neutral-50': '#5f717b',
+    'color-neutral-75': '#3c494f',
+    'color-neutral-90': '#283034',
+    'font-family-main': 'inherit',
+    'font-weight-light': '200',
+    'font-weight-regular': '400',
+    'font-weight-bold': '700',
+  },
 }
