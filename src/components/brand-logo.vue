b9f6f1d6923f332521485d21c9bbe47e6373583b
Laura Martinez Garcia <lauram@empathy.co>
2026-04-30 13:23:20 +0200

    feat: refactor layouts to accept the customer logo

---
+++ src/components/brand-logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <a v-if="logo" href="/" class="x-brand-logo">
+    <img :src="logo" alt="Brand logo" class="x-brand-logo__image" />
+  </a>
+</template>
+<script setup lang="ts">
+import type { ComputedRef } from 'vue'
+import { useExperienceControls } from '../composables/use-experience-controls.composable'
+
+const { getControlFromPath } = useExperienceControls()
+const logo = getControlFromPath('logo') as ComputedRef<string>
+</script>
+<style scoped>
+img {
+  width: auto;
+  max-width: 142px;
+}
+</style>
--- src/components/desktop/desktop-header-floating-predictive.vue
+++ src/components/desktop/desktop-header-floating-predictive.vue
@@ -1,7 +1,7 @@
 <template>
   <MaxDesktopWidthItem class="xds:pb-8">
     <header class="xds:grid xds:grid-cols-6 xds:items-center xds:gap-12 xds:pt-24">
-      <Logo />
+      <BrandLogo />
 
       <div class="xds:col-span-4 xds:flex xds:flex-col xds:gap-16">
         <div class="xds:relative">
@@ -23,7 +23,7 @@
 
 <script setup lang="ts">
 import { CloseMainModal, CrossIcon, LocationProvider } from '@empathyco/x-components'
-import Logo from '../logo.vue'
+import BrandLogo from '../brand-logo.vue'
 import MaxDesktopWidthItem from '../max-desktop-width-item.vue'
 import PredictiveLayer from '../predictive-layer/predictive-layer.vue'
 import SearchBox from '../search-box.vue'
