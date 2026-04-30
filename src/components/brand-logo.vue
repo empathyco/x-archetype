da6f1b3addb6ecba15d337a5287d629098b52b65
Laura Martinez Garcia <lauram@empathy.co>
2026-04-30 13:57:00 +0200

    feat: refactor layouts to accept the customer logo

--- src/components/brand-logo.vue
+++ src/components/brand-logo.vue
@@ -1,6 +1,6 @@
 <template>
   <a v-if="logo" href="/" class="x-brand-logo">
-    <img :src="logo" alt="Brand logo" class="x-brand-logo__image" />
+    <img :src="logo" alt="Brand logo" class="xds:w-auto xds:!max-w-[142px]" />
   </a>
 </template>
 <script setup lang="ts">
@@ -10,9 +10,3 @@ import { useExperienceControls } from '../composables/use-experience-controls.co
 const { getControlFromPath } = useExperienceControls()
 const logo = getControlFromPath('logo') as ComputedRef<string>
 </script>
-<style scoped>
-img {
-  width: auto;
-  max-width: 142px;
-}
-</style>
