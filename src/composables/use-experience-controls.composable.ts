ab8e175b0d39567af98aa496c518a9f6d5361e00
Carlos <57149778+C4rlos99@users.noreply.github.com>
2026-06-04 13:35:20 +0200

    chore: revert test changes

--- src/composables/use-experience-controls.composable.ts
+++ src/composables/use-experience-controls.composable.ts
@@ -1,3 +1,4 @@
+import type { Dictionary } from '@empathyco/x-utils'
 import type { ComputedRef } from 'vue'
 import { useState } from '@empathyco/x-components'
 import { getSafePropertyChain } from '@empathyco/x-utils'
@@ -15,14 +16,18 @@ export const useExperienceControls = (): {
   getControlFromPath: <SomeType>(path: string, defaultValue?: SomeType) => ComputedRef<SomeType>
 } => {
   const { controls } = useState('experienceControls')
-  const experienceControls = computed(() => controls.value ?? xControlsState)
+  const experienceControls = computed(() => controls.value?.controls ?? xControlsState.controls)
 
   const getControlFromPath = <SomeType>(
     path: string,
     defaultValue?: SomeType,
   ): ComputedRef<SomeType> => {
     return computed(() => {
-      return getSafePropertyChain(experienceControls.value, path, defaultValue) as SomeType
+      return getSafePropertyChain(
+        experienceControls.value as Dictionary<unknown>,
+        path,
+        defaultValue,
+      ) as SomeType
     })
   }
 
