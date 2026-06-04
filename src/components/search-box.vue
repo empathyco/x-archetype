3892b5f1d98dd5c6e90007ce9e3d432ae71420cc
Laura Martinez Garcia <lauram@empathy.co>
2026-06-04 13:29:09 +0200

    feat: style search-box component

--- src/components/search-box.vue
+++ src/components/search-box.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="xds:input-group xds:rounded-sm xds:input-group-lead">
+  <div class="xds:input-group xds:input-group-line xds:rounded-sm xds:input-group-lead">
+    <div class="xds:ml-0!">
+      <SearchIcon class="xds:icon-lg" />
+    </div>
+
     <SearchInput
       ref="searchInputRef"
       :autofocus="false"
@@ -8,11 +12,6 @@
       class="xds:input xds:desktop:pl-24!"
     />
 
-    <button class="xds:input-group-button xds:rounded-full" @click="toggleVoiceRecognition">
-      <span v-if="isListening">🛑</span>
-      <span v-else>🎤</span>
-    </button>
-
     <ClearSearchInput
       v-if="isDesktopOrGreater && x.query.searchBox"
       class="xds:input-group-button xds:input-group-button-rectangle xds:hover:bg-neutral-10"
@@ -20,14 +19,10 @@
       {{ $t('searchBox.clear') }}
     </ClearSearchInput>
 
-    <SearchButton
-      v-if="isDesktopOrGreater || !x.query.searchBox"
-      class="xds:input-group-button-primary xds:input-group-button xds:p-0"
+    <ClearSearchInput
+      v-if="!isDesktopOrGreater && x.query.searchBox"
+      class="xds:input-group-button-primary xds:input-group-button"
     >
-      <SearchIcon />
-    </SearchButton>
-
-    <ClearSearchInput v-else class="xds:input-group-button-primary xds:input-group-button">
       <CrossTinyIcon class="xds:icon-lg" />
     </ClearSearchInput>
   </div>
@@ -35,13 +30,11 @@
 
 <script setup lang="ts">
 import { CrossTinyIcon, SearchIcon, use$x } from '@empathyco/x-components'
-import { ClearSearchInput, SearchButton, SearchInput } from '@empathyco/x-components/search-box'
+import { ClearSearchInput, SearchInput } from '@empathyco/x-components/search-box'
 import { ref } from 'vue'
 import { useDevice } from '../composables/use-device.composable'
-import { useSpeechRecognition } from '../composables/use-speech-recognition-composable'
 
 const { isDesktopOrGreater } = useDevice()
 const x = use$x()
 const searchInputRef = ref<InstanceType<typeof SearchInput>>(null as any)
-const { isListening, toggleVoiceRecognition } = useSpeechRecognition(searchInputRef)
 </script>
