0edf3aa0bbdf7038eb7b61cc0642c752e0f158e5
Víctor CG <victorcg@empathy.co>
2026-06-10 15:44:05 +0200

    fix: handle missing instance extension gracefully by providing a default result component

--- src/composables/use-instance-extensions.composable.ts
+++ src/composables/use-instance-extensions.composable.ts
@@ -1,19 +1,21 @@
 import type { SnippetConfig } from '@empathyco/x-components'
 import type { Component } from 'vue'
 import { computed, defineAsyncComponent, inject } from 'vue'
+import DefaultResult from '../instance-extensions/empathy/result.vue'
 
 interface InstanceExtensionModule {
-  Result: Component
+  Result?: Component
 }
 
 export function useInstanceExtensions() {
   const snippetConfig = inject<SnippetConfig>('snippetConfig')!
   const resultComponent = computed(() =>
-    defineAsyncComponent(async () =>
-      import(`../instance-extensions/${snippetConfig.instance}.ts`).then(
-        (m: InstanceExtensionModule) => m.Result,
-      ),
-    ),
+    defineAsyncComponent(async () => {
+      const m = (await import(`../instance-extensions/${snippetConfig.instance}.ts`).catch(
+        () => ({}),
+      )) as InstanceExtensionModule
+      return m.Result ?? DefaultResult
+    }),
   )
 
   return { resultComponent }
