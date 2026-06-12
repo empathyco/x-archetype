dea7f154cf05f5037e832ad60eb4b3e283e333c6
Carlos Domínguez <57149778+C4rlos99@users.noreply.github.com>
2026-06-12 11:13:07 +0200

    fix(use-instance-extensions): set loading result component to avoid scroll problems (#818)

---
+++ src/composables/use-instance-extensions.composable.spec.ts
@@ -0,0 +1,53 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import BaseResult from '../components/results/base-result.vue'
+
+const PerfumesclubResult = { name: 'PerfumesclubResult', template: '<div>Mock Result</div>' }
+
+const snippetConfigStub = { instance: 'perfumesclub' }
+const defineAsyncComponentMock = vi.fn()
+vi.mock('vue', async importOriginal => {
+  const actual = await importOriginal<typeof import('vue')>()
+  return {
+    ...actual,
+    inject: vi.fn((key: string) => {
+      if (key === 'snippetConfig') {
+        return snippetConfigStub
+      }
+      return undefined
+    }),
+    defineAsyncComponent: defineAsyncComponentMock,
+  }
+})
+
+vi.mock('../components/results/base-result.vue', () => ({
+  default: 'BaseResult',
+}))
+
+vi.mock('../instance-extensions/perfumesclub.ts', () => ({
+  Result: PerfumesclubResult,
+}))
+
+describe('useInstanceExtensions composable', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+    vi.restoreAllMocks()
+  })
+
+  it('should set up the result componet correctly', async () => {
+    const { useInstanceExtensions } = await import('./use-instance-extensions.composable')
+    useInstanceExtensions()
+
+    expect(defineAsyncComponentMock).toHaveBeenCalledTimes(1)
+
+    // eslint-disable-next-line ts/no-unsafe-assignment
+    const options = defineAsyncComponentMock.mock.calls[0][0]
+    expect(options).toHaveProperty('loader')
+    expect(options).toHaveProperty('loadingComponent', BaseResult)
+    expect(options).toHaveProperty('errorComponent', BaseResult)
+    expect(options).toHaveProperty('timeout', 3000)
+
+    // eslint-disable-next-line ts/no-unsafe-assignment, ts/no-unsafe-member-access, ts/no-unsafe-call
+    const loaderResult = await options.loader()
+    expect(loaderResult).toBe(PerfumesclubResult)
+  })
+})
