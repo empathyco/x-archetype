6f4ae21d7637e5d0b9f80e85f836a85df66d7275
Carlos <57149778+C4rlos99@users.noreply.github.com>
2026-06-12 10:15:08 +0200

    fix(use-instance-extensions): set loading result component to avoid scroll problems

--- src/composables/use-instance-extensions.composable.ts
+++ src/composables/use-instance-extensions.composable.ts
@@ -1,6 +1,7 @@
 import type { SnippetConfig } from '@empathyco/x-components'
 import type { Component } from 'vue'
-import { computed, defineAsyncComponent, inject } from 'vue'
+import { defineAsyncComponent, inject } from 'vue'
+import BaseResult from '../components/results/base-result.vue'
 
 interface InstanceExtensionModule {
   Result: Component
@@ -8,13 +9,16 @@ interface InstanceExtensionModule {
 
 export function useInstanceExtensions() {
   const snippetConfig = inject<SnippetConfig>('snippetConfig')!
-  const resultComponent = computed(() =>
-    defineAsyncComponent(async () =>
-      import(`../instance-extensions/${snippetConfig.instance}.ts`)
-        .then((m: InstanceExtensionModule) => m.Result)
-        .catch(async () => import('../components/results/base-result.vue')),
-    ),
-  )
+
+  const resultComponent = defineAsyncComponent({
+    loader: async () =>
+      import(`../instance-extensions/${snippetConfig.instance}.ts`).then(
+        (m: InstanceExtensionModule) => m.Result,
+      ),
+    loadingComponent: BaseResult,
+    errorComponent: BaseResult,
+    timeout: 3000,
+  })
 
   return { resultComponent }
 }
